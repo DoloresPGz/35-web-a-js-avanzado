b3eb9ea240c7bc9553424e8ab1799f60ee117d4c
DoloresPGz <dolores.pgz@gmail.com>
2024-08-08 19:42:05 -0600

    ejemplos de task runners

---
+++ 1.2.1-Task-runners/index.js
@@ -0,0 +1,26 @@
+//TASK RUNNERS
+/*
+    Los task runners en npm me van a ayudar automatizar tareas repetitivas durante el proceso de el desarrollo
+    de mis proyectos.
+    Para ejecutar un scpript tengo que seguir los siguientes pasos:
+    1. Definir el script en package.json suguiendo el formato "nombre" : "script a ejecutar"
+        ejemplo: "dev": "npx nodemon index.js"
+    2. Ejecutar el script en consola siguiendo el formato: npm run <script>
+
+    Datos importantes: 
+    Puedo ejecutar el script de la misma manera con las siguientes alternativas:
+    npm run-script <script>
+    npm rum <script>
+    npm urn <script>
+*/
+
+
+console.log("Hola mundo")
+console.log("hice un nuevo cambio")
+console.log("Aquí va otro mensaje");
+
+//Este código crea un ciclo infinito que puedo detener en consola con CTRL + C
+// while(true){
+//     console.log("Estoy loco");
+    
+// }
